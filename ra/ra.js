18093635560fe3fa5b97d8139b93f01b21921d4c
francisco correa <themrcisco@gmail.com>
2017-09-19 18:13:45 -0500

    added inital part of tarea 1

--- ra/ra.js
+++ ra/ra.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const homeRoutes = require('./routes/home/home.js');
 const app = express();
 const handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
      extended: true
- }));
+}));
 
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
@@ -16,29 +16,15 @@ app.set('view engine','handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-
-
-
-
-app.get('/', function(req,res) {
-    res.redirect('/about');
-});
+app.use('/', homeRoutes);
 
 app.post('/reg', function(req,res) {
     console.log(req.body);
     res.redirect('/welcome');
 });
 
-app.get('/home', function(req,res) {
-    res.render('home');
-});
-
 app.get('/about', function(req,res) {
     res.render('about');
-});
-
-app.get('/welcome', function(req,res) {
-    res.render('welcome');
 });
 
 app.listen(app.get('port'), function() {
